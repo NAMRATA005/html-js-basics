9a836e7575cea8cbcf3092025c082edb0647e2c0
NAMRATA005 <chakrabortynamrata95@gmail.com>
2021-07-22 10:02:55 +0530

    scopes

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 
 a = 5;  // in use strict unassigned variables are not allowed
 console.log(a,"js printed");
+
+// scope of let var and const
+
+// 1.var
+
+// example 1:
+var greeter = "Namrata";
+    var greeter = "any name";
+    greeter = "name without assigntment"
+    function newFunction() {
+        var name = "horcrux";
+    }
+    console.log(name,"Test Name 1",greeter);
+
+// example2:
+
+    color = "blue";
+    count = 4;
+    if(count>3){
+        color = "green"; 
+    }
+
+    console.log(color,"final color");
+
+
+// 2. let 
+
+let color = "blue";
+let count = 4;
+    if(count>3){
+        let color = "green"; 
+    }
+
+    console.log(color,"final color with let");
