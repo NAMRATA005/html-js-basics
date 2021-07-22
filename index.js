7343d5d856aaa409e3dec4460c2b7820b0d2fbc7
NAMRATA005 <chakrabortynamrata95@gmail.com>
2021-07-22 10:16:26 +0530

    varible with same name--different scope

--- index.js
+++ index.js
@@ -7,31 +7,52 @@ console.log(a,"js printed");
 // 1.var
 
 // example 1:
-var greeter = "Namrata";
-    var greeter = "any name";
-    greeter = "name without assigntment"
-    function newFunction() {
-        var name = "horcrux";
-    }
-    console.log(name,"Test Name 1",greeter);
+// var greeter = "Namrata";
+//     var greeter = "any name";
+//     greeter = "name without assigntment"
+//     function newFunction() {
+//         var name = "horcrux";
+//     }
+//     console.log(name,"Test Name 1",greeter);
 
 // example2:
 
-    color = "blue";
-    count = 4;
+    var colorForScope = "blue";
+    var count = 4;
     if(count>3){
-        color = "green"; 
+        colorForScope = "green"; 
     }
 
-    console.log(color,"final color");
+    console.log(colorForScope,"final color");
 
 
 // 2. let 
 
-let color = "blue";
-let count = 4;
-    if(count>3){
-        let color = "green"; 
+let color2 = "blue";
+let countForScope = 4;
+    if(countForScope>3){
+        let color2 = "green"; 
     }
 
-    console.log(color,"final color with let");
+    console.log(color2,"final color with let");
+
+// 3. Variable with same name
+
+let countForHouse = 2;
+let name = "snape";
+
+if(countForHouse > 1){
+    let name = "slytherin";
+    console.log(name,"1.");
+
+    if(countForHouse ===2 ){
+        let name = "dumbledore";
+        console.log(name,"2.")
+    }
+    if(countForHouse < 3){
+        let name = "Lord Voldemort";
+        console.log(name,"3.")
+    }
+
+    console.log("print final name here :",name)
+}
